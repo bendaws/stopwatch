6ea5b084eb275ba74ddf5a6e455e2473b209dcf4
Ben Daws <ben@bendaws.net>
2025-11-06 14:36:49 -0600

    simple tostring() and concat to make formatting better

--- stopwatch.lua
+++ stopwatch.lua
@@ -1,5 +1,50 @@
---// Title: stopwatch.lua
---// Description: stopwatch implementation
+--[[===========================================================================
+                                  Stopwatch
+===============================================================================
+Basic stopwatch library for Lua.
+
+You can create a new stopwatch object with stopwatch.create(). You can either
+call the stopwatch as a function or do stopwatch_obj:start() and
+stopwatch_obj:stop() to end the stopwatch.
+===============================================================================
+                                Usage Example
+===============================================================================
+        local stlib = require("stopwatch")
+        local stopwatch = stlib.create()
+
+        stopwatch:start() -- or stopwatch() to toggle starting / stopping
+
+        local endAt = os.time() + 3
+        repeat until os.time() > endAt
+
+        stopwatch:stop() -- returns integer value of the stopwatch time
+
+        print(tostring(stopwatch)) -- * secs
+===============================================================================
+                                 License
+===============================================================================
+                               MIT License
+
+Copyright (c) 2025 Ben Daws
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+]]
 
 local stopwatch = {}
 
@@ -52,7 +97,7 @@ function stopwatch.create()
         end,
 
         __tostring = function(t)
-            return ("%s secs"):format(t.difference)
+            return tostring(t.difference)
         end,
 
         __len = function(t)
